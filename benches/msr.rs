be1da5d67018e75341f646b34a8833007f883472
Rens Hijdra <rhijdra2@hotmail.com>
2023-07-07 18:55:31 +0200

    ignore criterion 0.5 for now
    mend

--- benches/msr.rs
+++ benches/msr.rs
@@ -1,29 +1,28 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(criterion::runner)]
-extern crate energy_profiler;
 
-use energy_profiler::msr::profiler::{cmd_rdmsr, read_single_core_msr, read_single_core_msr_file};
 
 use criterion::Criterion;
 use criterion_macro::criterion;
-use energy_profiler::msr::measurement::Energy;
 
-// #[criterion]
-// pub fn bench_asm_rdmsr(c: &mut Criterion) {
-//     c.bench_function("asm_rdmsr", |b| b.iter(|| { read_single_core_msr(0) }));
+#[criterion]
+pub fn bench_asm_rdmsr(c: &mut Criterion) {
+    let mut group = c.benchmark_group("asm");
+    group.sampling_mode(criterion::SamplingMode::Flat);
+    group.bench_function("asm_rdmsr", |b| b.iter(|| { /*read_single_core_msr(0)*/ }));
+}
+
+// fn generate_criterion() -> Criterion<Energy> {
+//     Criterion::default().with_measurement(Energy)
 // }
 
-fn generate_criterion() -> Criterion<Energy> {
-    Criterion::default().with_measurement(Energy)
-}
-
-#[criterion(generate_criterion())]
-pub fn bench_cmd_rdmsr(c: &mut Criterion<Energy>) {
-    c.bench_function("cmd_rdmsr", |b| b.iter(|| { cmd_rdmsr() }));
-}
-
-#[criterion(generate_criterion())]
-pub fn bench_file_msr(c: &mut Criterion<Energy>) {
-    c.bench_function("file_rdmsr", |b| b.iter(|| { read_single_core_msr_file(0) }));
-}
+// #[criterion(generate_criterion())]
+// pub fn bench_cmd_rdmsr(c: &mut Criterion<Energy>) {
+//     c.bench_function("cmd_rdmsr", |b| b.iter(|| { cmd_rdmsr() }));
+// }
+//
+// #[criterion(generate_criterion())]
+// pub fn bench_file_msr(c: &mut Criterion<Energy>) {
+//     c.bench_function("file_rdmsr", |b| b.iter(|| { read_single_core_msr_file(0) }));
+// }
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Criterion-energy
+This is an extension crate for the criterion benchmarking framework.
+The goal of this crate is to measure energy consumption of benchmarks.
